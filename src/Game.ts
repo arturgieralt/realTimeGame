960e58ebfc448d091cd7514b4a3df9c827eff1a7
GIERI <gieralt.artur@gmail.com>
2018-11-20 00:35:04 +0100

    cannon working

--- src/Game.ts
+++ src/Game.ts
@@ -20,17 +20,6 @@ export class GameArea {
     }
 
     public buildElements() {
-        this.canvasArea.context.beginPath();
-        this.canvasArea.context.rect(0, 0, 50, 50);
-        this.canvasArea.context.fillStyle = "#FF0000";
-        this.canvasArea.context.fill();
-        this.canvasArea.context.closePath();
-        
-        this.canvasArea.context.beginPath();
-        this.canvasArea.context.rect(160, 10, 100, 40);
-        this.canvasArea.context.strokeStyle = "rgba(0, 0, 255, 0.5)";
-        this.canvasArea.context.stroke();
-        this.canvasArea.context.closePath();
     }
 
     public drawBall(ball: Ball) {
@@ -42,9 +31,12 @@ export class GameArea {
     }
 
     public drawCannon (){
+        this.clearArea();
         this.canvasArea.context.save();
-        this.canvasArea.context.rotate(this.angle*Math.PI/180);
-        this.canvasArea.context.drawImage(this.img as HTMLImageElement,590,-265, 50, 50);
+        this.canvasArea.context.translate(240,615);
+       this.canvasArea.context.rotate(this.angle*Math.PI/180);
+        // this.canvasArea.context.translate(-240,-240);
+        this.canvasArea.context.drawImage(this.img as HTMLImageElement, -25, -25, 50, 50);
         this.canvasArea.context.restore();
 
     }
@@ -52,10 +44,10 @@ export class GameArea {
     public attachKeyboardHandlers () {
         window.addEventListener('keydown', (e) => {
             if(e.keyCode == 39) {
-                this.recalculateCannonPosition(-1);
+                this.recalculateCannonPosition(3);
             }
             else if(e.keyCode == 37) {
-                this.recalculateCannonPosition(1);
+                this.recalculateCannonPosition(-3);
             }
         }
         )
@@ -65,9 +57,11 @@ export class GameArea {
       if((angleDelta < 0 && this.angle > 0 )|| (angleDelta > 0 && this.angle < 180 ) ) {
           this.angle += angleDelta;
           this.canvasArea.context.save();
-          this.canvasArea.context.rotate(this.angle*Math.PI/180);
-          this.canvasArea.context.drawImage(this.img as HTMLImageElement,590,-265, 50, 50);
-          this.canvasArea.context.restore();
+          this.canvasArea.context.translate(240,615);
+          this.canvasArea.context.rotate(this.angle * Math.PI/180);
+        //    this.canvasArea.context.translate(-240,-240);
+        this.canvasArea.context.drawImage(this.img as HTMLImageElement, -25, -25, 50, 50);
+           this.canvasArea.context.restore();
       }  
         
     }
